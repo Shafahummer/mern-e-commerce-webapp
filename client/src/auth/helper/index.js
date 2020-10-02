a25a2e1e9773530e155662877e3dce7eb736e13d
shafahummer11 <shafahummer11@gmail.com>
2020-10-02 18:35:29 +0530

    new commit

--- client/src/auth/helper/index.js
+++ client/src/auth/helper/index.js
@@ -60,4 +60,3 @@ export const isAuthenticated = () => {
         return false;
     }
 }
-
--- client/src/core/helper/imageHelper.js
+++ client/src/core/helper/imageHelper.js
@@ -5,6 +5,7 @@ const API = "https://mern-e-commerce-webapp.herokuapp.com/api"
 
 const ImageHelper = ({ product }) => {
     const imageUrl = product ? `${API}/product/photo/${product._id}` : 'https://media.istockphoto.com/photos/gadsisar-sagar-lake-in-jaisalmer-rajasthan-sunrise-at-gadsisar-sagar-picture-id1222290004'
+    console.log("image url---->", product)
     return (
         <div className="rounded border border-success p-2">
             <img
